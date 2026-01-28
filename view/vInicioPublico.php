8898bfeb1aa9fb6f6c297ad135c129eca2145ba8
alvaro-gargon <alvaro.garcia.daw@gmail.com>
2026-01-28 11:40:58 +0100

    style - trying to make a caroussel

--- view/vInicioPublico.php
+++ view/vInicioPublico.php
@@ -1,7 +1,24 @@
 <form method="post"> 
     <p id="arribaDerecha"><button class="login" name="LOGIN">LOGIN</button></p>
-    </form>
+</form>
 <h2 class="mensaje">Bienvenido al inicio público de la aplicacion</h2>
-<img class="centrarImagen" src="webroot/images/imagenNavegacion.png" alt="imagenNavegacion">
+<div class="container">
+    <div class="caroussel">
+        <input type="radio" name="rd" id="rd1">
+        <input type="radio" name="rd" id="rd2">
+        <input type="radio" name="rd" id="rd3">
+        <input type="radio" name="rd" id="rd4">
+        <input type="radio" name="rd" id="rd5">
+
+        <div class="photos">
+            <img src="http://picsum.photos/id/400/600/400" alt="">
+            <img src="http://picsum.photos/id/402/600/400" alt="">
+            <img src="http://picsum.photos/id/403/600/400" alt="">
+            <img src="http://picsum.photos/id/404/600/400" alt="">
+            <img src="http://picsum.photos/id/405/600/400" alt="">
+        </div>
+    </div>
+</div>
+
 
     
