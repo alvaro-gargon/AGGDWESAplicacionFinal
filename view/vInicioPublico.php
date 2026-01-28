8693fa940f8c76a71fb697c9a9666a288b1eb174
alvaro-gargon <alvaro.garcia.daw@gmail.com>
2026-01-28 13:11:29 +0100

    style - still trying to make a caroussel

--- view/vInicioPublico.php
+++ view/vInicioPublico.php
@@ -3,21 +3,15 @@
 </form>
 <h2 class="mensaje">Bienvenido al inicio público de la aplicacion</h2>
 <div class="container">
+    <button class="btn prev">←</button>
     <div class="caroussel">
-        <input type="radio" name="rd" id="rd1">
-        <input type="radio" name="rd" id="rd2">
-        <input type="radio" name="rd" id="rd3">
-        <input type="radio" name="rd" id="rd4">
-        <input type="radio" name="rd" id="rd5">
-
-        <div class="photos">
-            <img src="http://picsum.photos/id/400/600/400" alt="">
-            <img src="http://picsum.photos/id/402/600/400" alt="">
-            <img src="http://picsum.photos/id/403/600/400" alt="">
-            <img src="http://picsum.photos/id/404/600/400" alt="">
-            <img src="http://picsum.photos/id/405/600/400" alt="">
-        </div>
+        <img src="./webroot/images/arbolNavegacion.jpg" alt="arbolNavegacion">
+        <img src="./webroot/images/usoDeLaSession.png" alt="">
+        <img src="http://picsum.photos/id/403/600/400" alt="">
+        <img src="http://picsum.photos/id/404/600/400" alt="">
+        <img src="http://picsum.photos/id/405/600/400" alt="">
     </div>
+    <button class="btn next">→</button>
 </div>
 
 
